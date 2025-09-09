d44a17de48ab21844fda3b393aa070f6f382bc83
AAP PARA CELULAR <brendor16@gmail.com>
2025-09-09 18:39:34 -0300

    Update index.tsx

--- lovable-whatsapp/src/pages/index.tsx
+++ lovable-whatsapp/src/pages/index.tsx
@@ -1,9 +1,1 @@
-import WhatsAppTest from "../components/WhatsAppTest";
 
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <WhatsAppTest />
-    </div>
-  );
-}
